25caa12a94d179efae16d70cc99cebec96f23970
Jennifer Morrisey <jimorrisey@mail.fhsu.edu>
2023-03-15 11:46:43 -0500

    Updated Database

--- config/Database.php
+++ config/Database.php
@@ -25,11 +25,12 @@
             try{
                 $this->conn = new PDO($dsn, $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $this->conn;
 
             } catch(PDOException $e) {
                 echo 'Connection Error: ' . $e->getMessage();
             }
+
+            return $this->conn;
         }
         }
     
