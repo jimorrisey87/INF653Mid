98be4f251c11071ce24dfb3eb7ca35cb371c5466
Jennifer Morrisey <jimorrisey@mail.fhsu.edu>
2023-03-16 08:51:48 -0500

    deleted the host= post= dbname=

--- config/Database.php
+++ config/Database.php
@@ -20,7 +20,7 @@
         //DB connect
         public function connect(){
             $this->conn = null;
-            $dsn = "pgsql:host={$this->host};{$this->port};{$this->dbname};";
+            $dsn = "pgsql:{$this->host};{$this->port};{$this->dbname};";
 
             try{
                 $this->conn = new PDO($dsn, $this->username, $this->password);
